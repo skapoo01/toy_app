55cef749a45f34eca720126986882fcc6afab76b
Siddharth Kapoor <Siddharth.Kapoor@tufts.edu>
2016-02-28 22:48:29 -0500

    changed index page to users listing

--- config/routes.rb
+++ config/routes.rb
@@ -54,6 +54,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  root 'application#hello' 
+  root 'users#index' 
 
 end
